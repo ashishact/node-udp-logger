b8300c8b4153e8c237a9c45dfabe7c1ccc12c9c3
Ashish Charan Tandi <act.jnv@gmail.com>
2019-05-03 09:34:10 +0530

    get IP

--- udp.js
+++ udp.js
@@ -1,6 +1,32 @@
+// GET IP
+
+var os = require('os');
+var ifaces = os.networkInterfaces();
+
+Object.keys(ifaces).forEach(function (ifname) {
+  var alias = 0;
+
+  ifaces[ifname].forEach(function (iface) {
+    if ('IPv4' !== iface.family || iface.internal !== false) {
+      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+      return;
+    }
+
+    if (alias >= 1) {
+      // this single interface has multiple ipv4 addresses
+      console.log(ifname + ':' + alias, iface.address);
+    } else {
+      // this interface has only one ipv4 adress
+      console.log(ifname, iface.address);
+    }
+    ++alias;
+  });
+});
+
+
+// --------------------creating a udp server --------------------
 var udp = require('dgram');
 const fs = require('fs');
-// --------------------creating a udp server --------------------
 
 // creating a udp server
 var server = udp.createSocket('udp4');
