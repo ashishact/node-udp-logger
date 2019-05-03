0e5974de7e95e7b228837f2f72ce90a84f114c57
Ashish Charan Tandi <act.jnv@gmail.com>
2019-05-03 10:18:01 +0530

    Publish Mobile IP

--- udp.js
+++ udp.js
@@ -2,26 +2,49 @@
 
 var os = require('os');
 var ifaces = os.networkInterfaces();
-
+let mobileIPAddress = "";
 Object.keys(ifaces).forEach(function (ifname) {
-  var alias = 0;
+    var alias = 0;
 
-  ifaces[ifname].forEach(function (iface) {
-    if ('IPv4' !== iface.family || iface.internal !== false) {
-      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
-      return;
-    }
+    ifaces[ifname].forEach(function (iface) {
+        if ('IPv4' !== iface.family || iface.internal !== false) {
+            // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+            return;
+        }
 
-    if (alias >= 1) {
-      // this single interface has multiple ipv4 addresses
-      console.log(ifname + ':' + alias, iface.address);
-    } else {
-      // this interface has only one ipv4 adress
-      console.log(ifname, iface.address);
-    }
-    ++alias;
-  });
+        if (alias >= 1) {
+            // this single interface has multiple ipv4 addresses
+            console.log(ifname + ':' + alias, iface.address);
+        } else {
+            // this interface has only one ipv4 adress
+            console.log(ifname, iface.address);
+            mobileIPAddress = iface.address; // assuming it happens to have only one IP address
+        }
+        ++alias;
+    });
 });
+
+let request = require('request');
+request({
+    url: "http://k.ai0.in/ip-discovery",
+    method: 'POST',
+    json: {
+        ip: mobileIPAddress,
+        server: true,
+        client: false,
+    }
+}, function (error, response, body) {
+    if (error) {
+        console.warn("error", body);
+    }
+    if (body  && body.success) {
+        console.log("SUCCESS: ", body.success);
+    }
+    else {
+        console.log(body);
+    }
+});
+
 
 
 // --------------------creating a udp server --------------------
@@ -32,16 +55,16 @@ const fs = require('fs');
 var server = udp.createSocket('udp4');
 
 // emits when any error occurs
-server.on('error',function(error){
-  console.log('Error: ' + error);
-  server.close();
+server.on('error', function (error) {
+    console.log('Error: ' + error);
+    server.close();
 });
 
 
 
 // make a dir at parent
 const KV_ACC_LOG_DIR = '../kv_acc_log';
-if (!fs.existsSync(KV_ACC_LOG_DIR)){
+if (!fs.existsSync(KV_ACC_LOG_DIR)) {
     fs.mkdirSync(KV_ACC_LOG_DIR);
 }
 
@@ -50,71 +73,71 @@ let writeStreams = {}; // each one for each device
 let addr_to_espid = {};
 
 // emits on new datagram msg
-server.on('message',function(msg,info){
+server.on('message', function (msg, info) {
     let addr = `udp:${info.address}:${info.port}`;
-    
-    if(msg.length == 28){
-        let s = String(msg.slice(0,4).readInt32LE() + ",").padEnd(10);
-        for(let i = 1; i <= 6; i++){
-            if(i == 6){
-                s+= msg.slice(i*4,i*4+4).readFloatLE().toFixed(6) + "\n";
+
+    if (msg.length == 28) {
+        let s = String(msg.slice(0, 4).readInt32LE() + ",").padEnd(10);
+        for (let i = 1; i <= 6; i++) {
+            if (i == 6) {
+                s += msg.slice(i * 4, i * 4 + 4).readFloatLE().toFixed(6) + "\n";
             }
-            else{
-                s+= String(msg.slice(i*4,i*4+4).readFloatLE().toFixed(6) + ",").padEnd(12);
+            else {
+                s += String(msg.slice(i * 4, i * 4 + 4).readFloatLE().toFixed(6) + ",").padEnd(12);
             }
-            if(i == 3) s+= "    ";
+            if (i == 3) s += "    ";
         }
 
 
         let espid = addr_to_espid[addr];
-        if(espid && writeStreams[espid]){
+        if (espid && writeStreams[espid]) {
             writeStreams[espid].write(s);
 
-            if(writeStreams[espid].kvMsgCount) writeStreams[espid].kvMsgCount++;
+            if (writeStreams[espid].kvMsgCount) writeStreams[espid].kvMsgCount++;
             else writeStreams[espid].kvMsgCount = 1;
             process.stdout.write(`${espid}: ${String(writeStreams[espid].kvMsgCount)}`.padEnd(50) + "\r");
         }
-        else{
+        else {
             // console.log(s);
         }
     }
-    else{
+    else {
         let utf_msg = msg.toString();
         console.log(utf_msg);
-        if(utf_msg.startsWith("start:")){
+        if (utf_msg.startsWith("start:")) {
             let m = utf_msg.match(/start:espid:([a-f0-9]{12})/);
-            if(m){
+            if (m) {
                 let espid = m[1];
                 addr_to_espid[addr] = espid;
 
-                if(writeStreams[espid]){
+                if (writeStreams[espid]) {
                     writeStreams[espid].end();
                 }
-                
+
                 let path = KV_ACC_LOG_DIR + `/${espid}-${new Date().toISOString()}.csv`;
                 writeStreams[espid] = fs.createWriteStream(path);
 
             }
-            else{
+            else {
                 console.log(utf_msg);
             }
         }
-        else if(utf_msg.startsWith("stop:")){
+        else if (utf_msg.startsWith("stop:")) {
             let m = utf_msg.match(/stop:espid:([a-f0-9]{12})/);
-            if(m){
+            if (m) {
                 let espid = m[1];
-                
-                if(writeStreams[espid]){
+
+                if (writeStreams[espid]) {
                     writeStreams[espid].end();
                 }
-                
+
                 delete addr_to_espid[addr]; // because it will stop, the ip will be given to another dev
             }
-            else{
+            else {
                 console.log(utf_msg);
             }
         }
-        else{
+        else {
             console.log(utf_msg);
         }
         // invalid message
@@ -123,23 +146,23 @@ server.on('message',function(msg,info){
 });
 
 //emits when socket is ready and listening for datagram msgs
-server.on('listening',function(){
-  var address = server.address();
-  var port = address.port;
-  var family = address.family;
-  var ipaddr = address.address;
-  console.log('Server is listening at port' + port);
-  console.log('Server ip :' + ipaddr);
-  console.log('Server is IP4/IP6 : ' + family);
+server.on('listening', function () {
+    var address = server.address();
+    var port = address.port;
+    var family = address.family;
+    var ipaddr = address.address;
+    console.log('Server is listening at port' + port);
+    console.log('Server ip :' + ipaddr);
+    console.log('Server is IP4/IP6 : ' + family);
 });
 
 //emits after the socket is closed using socket.close();
-server.on('close',function(){
-  console.log('Socket is closed !');
+server.on('close', function () {
+    console.log('Socket is closed !');
 });
 
 server.bind(5555, "0.0.0.0");
 
-setTimeout(function(){
+setTimeout(function () {
     server.close();
-},24 * 60 * 60 * 1000); // 1 day
+}, 24 * 60 * 60 * 1000); // 1 day
